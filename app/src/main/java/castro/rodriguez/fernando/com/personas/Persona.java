f2b0bc24c13f68d25446926747282006c961da60
Fernando <Fejota001@hotmail.com>
2017-04-04 21:11:33 -0500

    Clases Javas

---
+++ app/src/main/java/castro/rodriguez/fernando/com/personas/Persona.java
@@ -0,0 +1,63 @@
+package castro.rodriguez.fernando.com.personas;
+
+/**
+ * Created by android on 04/04/2017.
+ */
+
+public class Persona {
+
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String pasatiempo;
+
+    public Persona(String nombre, String apellido, String pasatiempo, int edad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.pasatiempo = pasatiempo;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getPasatiempo() {
+        return pasatiempo;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setPasatiempo(String pasatiempo) {
+        this.pasatiempo = pasatiempo;
+    }
+
+
+    public void guardar (){
+
+        Datos.guardar(this);
+
+    }
+
+
+
+
+}
